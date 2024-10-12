c51bf510987d082e022d311a9c1b860f3ac28667
mennazezo5 <168407693+mennazezo5@users.noreply.github.com>
2024-10-12 14:17:16 +0300

    Add files via upload

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<fstream>
+#include<time.h>
+#include"Funy.h"
+using namespace std;
+
+int main()
+{
+    
+    clearGameRepresent();
+     showMenu();
+    return 0;
+}
+
+
